35508b262ef25ba32a36318aa8a6229072be47fa
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-04 23:10:11 +0800

    categ_id

--- resources/views/admin/settings/security.blade.php
+++ resources/views/admin/settings/security.blade.php
@@ -3,7 +3,7 @@
         @include('partials._settings')
 
         <div style="width: 50%; margin: 1% 5%; display:inline-block; vertical-align: top;">
-            <h1>Meow This is Security Page</h1>
+            <h1>This is Security Page</h1>
         </div>
     </x-sidenav>
 </x-layout>
