999ea3e2ed65ae7f8a403adfa02f1d13aa1151be
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-04 09:26:24 +0800

    a

--- database/migrations/2022_10_28_021340_create_users_table.php
+++ database/migrations/2022_10_28_021340_create_users_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             // $table->foreignId('categ_id')->constrained('categs');
-            $table->json('categ_id')->foreign()->constrained('categs');
+            $table->jsonb('categ_id')->foreign()->constrained('categs');
             $table->rememberToken();
             $table->timestamps();
         });
