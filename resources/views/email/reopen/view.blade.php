9154e90c2d26fd55c91cc941ff3d6fe192e7ead9
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-09 16:30:58 +0800

    view tick

--- resources/views/email/reopen/view.blade.php
+++ resources/views/email/reopen/view.blade.php
@@ -43,7 +43,7 @@
                     @endif
                     <td style="word-break: break-all;">{{$ticket->user->email}}</td>
                     <td>
-                      <button class="reopen-button" onclick="location.href='/reopen/{{$ticket->id}}/{{$student->id}}';">Reopen</button>
+                      <button class="reopen-button" onclick="location.href='/reopen/{{$ticket->id}}/{{$ticket->student->id}}';">Reopen</button>
                     </td>
                   </tr>
     
