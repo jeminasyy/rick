49fd75f39e4dda798d4de9bc762085f435777b30
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-09 21:06:14 +0800

    er

--- resources/views/email/reopen/create.blade.php
+++ resources/views/email/reopen/create.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div id="reopen">
         <h1>{{$ticket->student->id}}</h1>
-        <form method="POST" action="/reopen/store/{{$ticket->id}}/{{$student->id}}">
+        <form method="POST" action="/reopen/store/{{$ticket->id}}/{{$ticket->student->id}}">
         </form>
     </div>
 </x-layout>
