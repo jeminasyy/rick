75fbb05d121adc75b866be8bee71b46c3a27a015
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-09 22:58:22 +0800

    er

--- resources/views/admin/tickets/show.blade.php
+++ resources/views/admin/tickets/show.blade.php
@@ -172,7 +172,7 @@
             </div>
         </div>
 
-        @if (count($ticket->rating) != 0)
+        @if ($ticket->rating)
             <div class="ticket-div">
                 <p style="font-size: 17px; font-weight:bold">Student Feedback</p>
                 <hr style="width: 100%; background-color: #C4C4C4; border: 0.1px solid #C4C4C4;
@@ -203,7 +203,7 @@
             </div>
         @endif
 
-        @if (count($ticket->reopens) != 0)
+        @if ($ticket->reopens)
             <div class="ticket-div">
                 @foreach($ticket->reopens as $reopen)
                     <br>
@@ -224,10 +224,11 @@
                     </p>
                 @endforeach
             </div>
+        @endif
 
-            {{-- Action Buttons --}}
+        @if (count($ticket->reopens) != 0)
             <div class="ticket-div" style="margin-top:150px">
-                @if ($rating->user_id == auth()->id())
+                @if ($ticket->user_id == auth()->id())
                     @if ($ticket->status == "New")
                         <div class="bottom">
                             <button type="submit" 
