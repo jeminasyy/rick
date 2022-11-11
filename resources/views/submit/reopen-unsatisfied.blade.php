cfa69f9cf83ac67a84e701011b4094fbfbacd163
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-11 21:35:56 +0800

    sd

--- resources/views/submit/reopen-unsatisfied.blade.php
+++ resources/views/submit/reopen-unsatisfied.blade.php
@@ -1,18 +1,18 @@
 <x-layout>
     <div class="form-inline3">
         <h1 style="color:black; font-family: 'Cabin', sans-serif; font-size: 29;text-align: center; margin-top: 13%; position: relative;">
-            You answere No.
+            You answered No.
         </h1>
         <h1 style="color:black; font-family: 'Cabin', sans-serif; font-size: 29;text-align: center;  position: relative;">
             Do you want to reopen this ticket?
         </h1>
-        <button
+        {{-- <button
             class="btn btn-secondary btn-lg"
             id="primary-button"
             onclick="location.href='/{{$ticket->id}}/ticket/resolve';"
         >
             Yes, reopen ticket
-        </button>
+        </button> --}}
         <button 
             class="btn btn-secondary btn-lg"
             id="secondary-button"
@@ -20,5 +20,12 @@
         >
             No, return home
         </button>    
+        <button 
+            class="btn btn-secondary btn-lg"
+            id="secondary-button"
+            onclick="location.href='/';"
+        >
+            No, return home
+        </button>  
     </div>
 </x-layout>
