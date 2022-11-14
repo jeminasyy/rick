f030cf43cbdc04ee8fd281da9e49b07039ff1438
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-14 13:00:24 +0800

    db

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-sidenav>
         <div class="col-md-12 grid-margin">
-            <p style="font-size: 17px; font-weight:bold">Dashboard</p>
+            <p style="font-size: 27px; font-weight:bold">Dashboard</p>
             <br>
             <hr style="width: 100%; background-color: #C4C4C4; border: 0.1px solid #C4C4C4;
             transform: rotate(0.08deg); ">
@@ -9,29 +9,29 @@
             <div class="row">
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6">
-                        <p class="attribute">Total Tickets</p>
-                        <p>4</p>
+                        <p class="card-label">Total Tickets</p>
+                        <p class="card-info">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6">
-                        <p class="attribute">New Tickets</p>
-                        <p>4</p>
+                        <p class="card-label">New Tickets</p>
+                        <p class="card-info">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6">
-                        <p class="attribute">Resolved Tickets</p>
-                        <p>4</p>
+                        <p class="card-label">Resolved Tickets</p>
+                        <p class="card-info">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6" >
-                        <p class="attribute">Reopened Tickets</p>
-                        <p>4</p>
+                        <p class="card-label">Reopened Tickets</p>
+                        <p class="card-info">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
