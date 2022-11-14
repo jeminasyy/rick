3d6249821b237a2286294a1942da9b4cdae5694e
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-14 12:16:50 +0800

    db

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,27 +1,29 @@
 <x-layout>
     <x-sidenav>
         <h1>Dashboaard</h1>
-        <div class="row">
-            <div class="col-md-3">
-                <div class="card card-body bg-primary text-white mb-3">
-                    <label>Total Tickets</label>
-                    <h1>4</h1>
-                    <a href="/", class="text-white">View</a>
-                </div>
-                <div class="card card-body bg-primary text-white mb-3">
-                    <label>New Tickets</label>
-                    <h1>4</h1>
-                    <a href="/", class="text-white">View</a>
-                </div>
-                <div class="card card-body bg-primary text-white mb-3">
-                    <label>Resolved Tickets</label>
-                    <h1>4</h1>
-                    <a href="/", class="text-white">View</a>
-                </div>
-                <div class="card card-body bg-primary text-white mb-3">
-                    <label>Re-opened Tickets</label>
-                    <h1>4</h1>
-                    <a href="/", class="text-white">View</a>
+        <div class="col-md-12 grid-margin">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card card-body bg-primary text-white mb-3">
+                        <label>Total Tickets</label>
+                        <h1>4</h1>
+                        <a href="/", class="text-white">View</a>
+                    </div>
+                    <div class="card card-body bg-primary text-white mb-3">
+                        <label>New Tickets</label>
+                        <h1>4</h1>
+                        <a href="/", class="text-white">View</a>
+                    </div>
+                    <div class="card card-body bg-primary text-white mb-3">
+                        <label>Resolved Tickets</label>
+                        <h1>4</h1>
+                        <a href="/", class="text-white">View</a>
+                    </div>
+                    <div class="card card-body bg-primary text-white mb-3">
+                        <label>Reopened Tickets</label>
+                        <h1>4</h1>
+                        <a href="/", class="text-white">View</a>
+                    </div>
                 </div>
             </div>
         </div>
