4de656cd8d642f5e740bd3cf102b6463ccc65043
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-14 13:09:18 +0800

    db

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,29 +9,29 @@
             <div class="row">
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6">
-                        <p class="font-weight-bold h3">Total Tickets</p>
-                        <p class="cardinfo">4</p>
+                        <p class="font-weight-bold h4">Total Tickets</p>
+                        <p class="h4">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6">
-                        <p class="font-weight-bold h3">New Tickets</p>
-                        <p class="cardinfo">4</p>
+                        <p class="font-weight-bold h4">New Tickets</p>
+                        <p class="h4">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6">
-                        <p class="font-weight-bold h3">Resolved Tickets</p>
-                        <p class="cardinfo">4</p>
+                        <p class="font-weight-bold h4">Resolved Tickets</p>
+                        <p class="h4">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6" >
-                        <p class="font-weight-bold h3">Reopened Tickets</p>
-                        <p class="cardinfo">4</p>
+                        <p class="font-weight-bold h4">Reopened Tickets</p>
+                        <p class="h4">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
