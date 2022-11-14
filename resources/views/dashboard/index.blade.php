4c13b362c68640a129a871d22664c74497deda60
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-14 13:06:38 +0800

    db

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,28 +9,28 @@
             <div class="row">
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6">
-                        <p class="cardlabel">Total Tickets</p>
+                        <p class="font-weight-bold h5">Total Tickets</p>
                         <p class="cardinfo">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6">
-                        <p class="cardlabel">New Tickets</p>
+                        <p class="font-weight-bold h5">New Tickets</p>
                         <p class="cardinfo">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6">
-                        <p class="cardlabel">Resolved Tickets</p>
+                        <p class="font-weight-bold h5">Resolved Tickets</p>
                         <p class="cardinfo">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
                 </div>
                 <div class="col-md-3">
                     <div class="card card-body border rounded-10 bg-white mb-3 p-6" >
-                        <p class="cardlabel">Reopened Tickets</p>
+                        <p class="font-weight-bold h5">Reopened Tickets</p>
                         <p class="cardinfo">4</p>
                         <a href="/" class="text-primary">View</a>
                     </div>
