f09acb35801f6defb4f2e3d84c55164f46ec4154
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-14 12:11:52 +0800

    db

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,14 @@
 <x-layout>
     <x-sidenav>
         <h1>Dashboaard</h1>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-body bg-primary text-white mb-3">
+                    <label>Total Orders</label>
+                    <h1>4</h1>
+                    <a href="/", class="text-white">View</a>
+                </div>
+            </div>
+        </div>
     </x-sidenav>
 </x-layout>
