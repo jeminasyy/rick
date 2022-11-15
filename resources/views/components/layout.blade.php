ae04fe4b9485035a5d8f8fc7f1faed6c510a919d
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-15 15:21:26 +0800

    ticket chart

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,7 +21,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <script src="https://cdn.tailwindcss.com"></script>
-        <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+        {{-- <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script> --}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
         <script>
             tailwind.config = {
                 theme: {
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,21 +8,7 @@
             <br>
 
             <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <div class="card">
-                            <div class="card-header">Dashboard</div>
-            
-                            <div class="card-body">
-            
-                                <h1>{{ $chart1->options['chart_title'] }}</h1>
-                                {!! $chart1->renderHtml() !!}
-            
-                            </div>
-            
-                        </div>
-                    </div>
-                </div>
+                <canvas id="myChart"></canvas>
             </div>
 
             <div class="row">
@@ -115,7 +101,7 @@
     </x-sidenav>
 </x-layout>
 
-@section('javascript')
+{{-- @section('javascript')
 {!! $chart1->renderChartJsLibrary() !!}
 {!! $chart1->renderJs() !!}
-@endsection
+@endsection --}}
