52550c7d47318bdbcc727f18132393260707624a
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-15 20:42:09 +0800

    me

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,11 +7,11 @@
         <br>
 
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-5">
                 <h1>{{ $chart1->options['chart_title'] }}</h1>
                 {!! $chart1->renderHtml() !!}
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-5">
                 <h1>{{ $chart2->options['chart_title'] }}</h1>
                 {!! $chart2->renderHtml() !!}
             </div>
