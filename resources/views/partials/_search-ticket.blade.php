6da9978cec84c0a9bcd4da77d662d3d05dbfe65f
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-16 12:38:31 +0800

    filter

--- resources/views/partials/_search-ticket.blade.php
+++ resources/views/partials/_search-ticket.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-lg">
+    <div class="col-lg-2">
         <form action="/tickets">
             <div class="relative border-2 border-gray-100 rounded-lg" id="search-div">
                 
@@ -25,7 +25,7 @@
 </div>
 
 <div class="row">
-    <div class="col-md-1">
+    <div class="col-2">
         <form action="/tickets">
             <p class="attribute">Category</p>
             <select name="categ_id" id="priority" onchange="this.form.submit()">
@@ -64,7 +64,7 @@
             </select>
         </form>
     </div>
-    <div class="col-md-1">
+    <div class="col-2">
         <form action="/tickets">
             <p class="attribute">Priority</p>
             <select name="priority" id="priority" onchange="this.form.submit()">
@@ -76,7 +76,7 @@
             </select>
         </form>
     </div>
-    <div class="col-md-1">
+    <div class="col-2">
         <form action="/tickets">
             <p class="attribute">Status</p>
             <select name="status" id="priority" onchange="this.form.submit()">
@@ -92,7 +92,7 @@
             </select>
         </form>
     </div>
-    <div class="col-md-1">
+    <div class="col-2">
         <form action="/tickets">
             <p class="attribute">Assignee</p>
             <select name="user_id" id="priority" onchange="this.form.submit()">
