de0b1a7b4e518ba7dd00a93e14d4829d34a3e9a2
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-16 12:18:25 +0800

    filter

--- resources/views/partials/_search-ticket.blade.php
+++ resources/views/partials/_search-ticket.blade.php
@@ -23,7 +23,7 @@
 </div>
 
 <div class="row">
-    <div class="col">
+    <div class="col-md-3">
         <form action="/tickets">
             <p class="attribute">Category</p>
             <select name="categ_id" id="priority" onchange="this.form.submit()">
@@ -62,7 +62,7 @@
             </select>
         </form>
     </div>
-    <div class="col">
+    <div class="col-md-3">
         <form action="/tickets">
             <p class="attribute">Priority</p>
             <select name="priority" id="priority" onchange="this.form.submit()">
@@ -74,7 +74,7 @@
             </select>
         </form>
     </div>
-    <div class="col">
+    <div class="col-md-3">
         <form action="/tickets">
             <p class="attribute">Assignee</p>
             <select name="user_id" id="priority" onchange="this.form.submit()">
