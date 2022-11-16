a0acc61ad0917b8f16ca2186bd6453be6019a5cb
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-16 16:46:33 +0800

    d

--- resources/views/partials/_ticket-nav.blade.php
+++ resources/views/partials/_ticket-nav.blade.php
@@ -1,8 +1,4 @@
-{{-- <div id="ticketNav"> --}}
-    <div id="ticketNav">
-        <ul>
-            <li><a class="active" href="/tickets">My Tickets</a></li>
-            <li><a href="#">All Tickets</a></li>
-          </ul>
-    </div>
-{{-- </div> --}}
+<div class="ticketNav">
+    <a class="active" href="/tickets">My Tickets</a>
+    <a href="#">All Tickets</a>
+</div>
