cf72adb5f4c620f91c68540c6475af4d8556e060
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-16 12:04:56 +0800

    filter

--- resources/views/partials/_filter-user.blade.php
+++ resources/views/partials/_filter-user.blade.php
@@ -1,11 +1,11 @@
 <form action="/tickets">
     <p class="attribute">Assignee</p>
-    <select name="user_id" id="priority" onchange="this.form.submit()" value="{{old('categ_id')}}">
+    <select name="user_id" id="priority" onchange="this.form.submit()" value="{{old('user_id')}}">
         <option value="" selected>All</option>
-        @unless(count($users) == 0)
+        {{-- @unless(count($users) == 0) --}}
         @foreach ($users as $user)
             <option value="{{$user['id']}}">{{$user['email']}}</option>
         @endforeach
-        @endunless
+        {{-- @endunless --}}
     </select>
 </form>
