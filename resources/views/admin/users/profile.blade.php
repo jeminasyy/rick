84d8e651ba52e699353cbd8f4e4aa0aa5d16d415
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-17 17:08:23 +0800

    dashboard

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -2,12 +2,37 @@
     <x-sidenav>
         <div style="width: 50%; margin: 1% 5%; display:inline-block; vertical-align: top;">
             <h1 style="font-size: 19px">Hello {{auth()->user()->firstName}} {{auth()->user()->lastName}}!</h1><br>
-            <p class="attribute">Role</p>
+            <p class="attribute">ROLE</p>
             <p>{{auth()->user()->role}}</p>
-            <p class="attribute">Email</p>
+            <p class="attribute">EMAIL</p>
             <p>{{auth()->user()->email}}</p>
-            <p class="attribute">Email Verified</p>
+            <p class="attribute">EMAIL VERIFIED</p>
             <p>{{auth()->user()->email_verified_at}}</p>
+            <p class="attribute">USER ACCESS</p>
+                <p class="attribute">Request</p>
+                @foreach (auth()->user()->usercategs() as $usercateg)
+                    @if($usercateg->categ()->type == 'Request')
+                        <p>{{$usercateg->categ()->name}}</p>
+                    @endif
+                @endforeach
+                <p class="attribute">Inquiries</p>
+                @foreach (auth()->user()->usercategs() as $usercateg)
+                    @if($usercateg->categ()->type == 'Inquiries')
+                        <p>{{$usercateg->categ()->name}}</p>
+                    @endif
+                @endforeach
+                <p class="attribute">Concerns</p>
+                @foreach (auth()->user()->usercategs() as $usercateg)
+                    @if($usercateg->categ()->type == 'Concerns')
+                        <p>{{$usercateg->categ()->name}}</p>
+                    @endif
+                @endforeach
+                <p class="attribute">Others</p>
+                @foreach (auth()->user()->usercategs() as $usercateg)
+                    @if($usercateg->categ()->type == 'Others')
+                        <p>{{$usercateg->categ()->name}}</p>
+                    @endif
+                @endforeach
         </div>
     </x-sidenav>
 </x-layout>
