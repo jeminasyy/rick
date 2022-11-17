cf328aa693970d998f01e7023f3c1e36b6072ce6
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-17 20:40:15 +0800

    show user

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-sidenav>
-        @include('partials._settings')
+        {{-- @include('partials._settings') --}}
         <div style="width: 50%; margin: 1% 5%; display:inline-block; vertical-align: top;">
             <header>
                 <h2 class="text-2xl font-bold uppercase mb-1 mb-8">
