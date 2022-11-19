d1b44dc2438f968ffbcefefaf3f217747b6ba051
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-19 18:30:15 +0800

    read

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -2,7 +2,7 @@
     <x-sidenav>
         @include('partials._settings')
         <div style="width: 50%; margin: 1% 5%; display:inline-block; vertical-align: top;">
-            <div style="float: right">
+            {{-- <div style="float: right">
                 <button class="editBtn" onclick="location.href='/user/{{$user->id}}/edit';"><i class='bx-fw bx bxs-edit-alt bx-sm'></i>Edit</button>
                 <form method="POST" action="/users/{{$user->id}}">
                     @csrf
@@ -10,7 +10,7 @@
                     <button type="submit" class="text-red-500"><i class='bx-fw bx bxs-trash-alt bx-sm' ></i>Delete</button>
                     {{-- <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button> --}}
                   </form>
-            </div>
+            </div> --}}
 
             <a href="/users">
                 <i class='bx bx-left-arrow-alt bx-md'></i>
@@ -49,6 +49,27 @@
                     <p>{{$usercateg->categ->name}}</p>
                 @endif
             @endforeach
+
+            <div class="form-group col-md-10" style="display: flex; flex-direction: row;">
+                <a href="/" class="btn btn-primary" style="margin-top: 4%; margin-left: 104%; background-color: #FFFFFF;
+                    border: 1px solid#F8CA0A;
+                    border-radius: 5px; color:#F8CA0A ;">
+                    Cancel
+                </a>
+                <button type="submit" class="btn btn-primary" style="margin-top: 4%; margin-left:2%; background-color: #F8CA0A;
+                    border: 1px solid#F8CA0A;
+                    border-radius: 5px; color:#000000 ;">
+                    Submit
+                </button>
+            <div>
+                <button class="editBtn" onclick="location.href='/user/{{$user->id}}/edit';"><i class='bx-fw bx bxs-edit-alt bx-sm'></i>Edit</button>
+                <form method="POST" action="/users/{{$user->id}}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="text-red-500"><i class='bx-fw bx bxs-trash-alt bx-sm' ></i>Delete</button>
+                    {{-- <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button> --}}
+                </form>
+            </div>
         </div>
     </x-sidenav>
 </x-layout>
