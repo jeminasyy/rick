7563ac12483b8f0b04e431fa7c55ec41c534c4e4
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-20 22:48:05 +0800

    notif menu

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -80,11 +80,7 @@
                     </button> --}}
                     <div class="dropdown">
                         <button onclick="myFunction()" class="dropbtn">Dropdown</button>
-                        <div id="myDropdown" class="dropdown-content">
-                          <a href="#home">Home</a>
-                          <a href="#about">About</a>
-                          <a href="#contact">Contact</a>
-                        </div>
+                        
                     </div>
                 </div>
 
@@ -108,6 +104,11 @@
         </nav>
 
         <main>
+            <div id="myDropdown" class="dropdown-content">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+              </div>
             {{-- VIEW OUTPUT --}}
                 {{$slot}}
         </main>
