7ac111f21b1eebce32061cedfed69a1292fa03ef
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-20 15:09:55 +0800

    export

--- app/Exports/TicketsExport.php
+++ app/Exports/TicketsExport.php
@@ -4,14 +4,19 @@ namespace App\Exports;
 
 use App\Models\Ticket;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
 
-class TicketsExport implements FromCollection
+class TicketsExport implements FromView
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+
+    public function view(): View
     {
-        return Ticket::all();
+        return view('admin.dashboard.tickets', [
+            'invoices' => Ticket::all()
+        ]);
     }
 }
---
+++ resources/views/dashboard/tickets.blade.php
@@ -0,0 +1,35 @@
+<table>
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">CATEGORY</th>
+        <th scope="col">REQUESTER</th>
+        <th scope="col">DESCRIPTION</th>
+        <th scope="col">DATE</th>
+        <th scope="col">STATUS</th>
+        <th scope="col">ASSIGNEE</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($tickets as $ticket)
+        <tr>
+          <td>{{$ticket->id}}</td>
+          <td>{{$ticket->categ->name}}</td>
+          <td scope="row" style="word-break: break-all;">
+            <b>{{$ticket->student->LName}}, {{$ticket->student->FName}}</b>
+            <br>{{$ticket->student->email}}
+          </td>
+          <td>{{$ticket->description}}</td>
+          <td>{{$ticket->created_at}}</td>
+          <td>{{$ticket->status}}</td>
+          @if ($ticket->user != null)
+            <td style="word-break: break-all;">{{$ticket->user->email}}</td>
+          @else
+            <td style="word-break: break-all;">Unavailable</td>
+          @endif
+          <td>
+            <a href="/tickets/{{$ticket->id}}"><i class='bx-fw bx bxs-show bx-sm'></i>View</a>
+          </td>
+        </tr>
+    </tbody>
+  </table>
