f185672be6de9dbb13d7154bf7448e548cd40b00
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-20 23:19:26 +0800

    notif menu

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -73,11 +73,11 @@
                     </span>
                     @if (auth()->user()->newNotifs > 0)
                         <button onclick="myFunction()" class="dropbtn">
-                            <i class='bx-fw bx bxs-bell bx-sm bx-tada'></i>
+                            <i class='bx-fw bx bxs-bell bx-md bx-tada'></i>
                         </button>
                     @else
                         <button onclick="myFunction()" class="dropbtn">
-                            <i class='bx-fw bx bxs-bell bx-sm'></i>
+                            <i class='bx-fw bx bxs-bell bx-md'></i>
                         </button>
                     @endif
                     
