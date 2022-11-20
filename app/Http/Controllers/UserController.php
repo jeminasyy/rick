e5a8e8a8414d6bef3e480bf85ae9084dc03f19bd
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-20 17:50:04 +0800

    reset pass

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -281,7 +281,7 @@ class UserController extends Controller
             $user = User::find($find->id);
             $formFields['resetToken'] = Str::random(42);
             $user->update($formFields);
-            dd($user->resetToken);
+            // dd($user->resetToken);
             Mail::to($user->email)->send(new ResetPassword($user));
             return redirect('/forgotpassword/sent');
         } else {
