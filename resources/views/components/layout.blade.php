da937d59b52f2c816281489fbc0c77f974f76f43
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-20 23:04:23 +0800

    notif menu

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -71,15 +71,13 @@
                     <span class="font-bold-uppercase">
                         Welcome {{auth()->user()->firstName}}
                     </span>
-                    {{-- <div class="dropdown"> --}}
-                        <button onclick="myFunction()" class="dropbtn">
-                            @if (auth()->user()->newNotifs > 0)
-                                <i class='bx-fw bx bxs-bell bx-md bx-tada'></i>
-                            @else
-                                <i class='bx-fw bx bxs-bell bx-md'></i>
-                            @endif
-                        </button>
-                    {{-- </div> --}}
+                    <button onclick="myFunction()" class="dropbtn">
+                        @if (auth()->user()->newNotifs > 0)
+                            <i class='bx-fw bx bxs-bell bx-md bx-tada'></i>
+                        @else
+                            <i class='bx-fw bx bxs-bell bx-md'></i>
+                        @endif
+                    </button>
                     {{-- <div class="dropdown">
                         <button onclick="myFunction()" class="dropbtn">Dropdown</button>
                         
