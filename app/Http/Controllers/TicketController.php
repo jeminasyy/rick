2765db80fce5d0127ec806a26475317feaf5b972
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-21 19:37:10 +0800

    sddf

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -181,7 +181,7 @@ class TicketController extends Controller
 
         if (count($users) == 0) {
             $admins = DB::table('users')->where('verified', true)->where('role', "Admin")->get()->toArray();
-
+            dd($admins);
             $min = DB::table('tickets')->where('user_id', $admins[0]->id)->whereNot('status', 'Resolved')->whereNot('status', 'Voided')->count();
             $min_id = $admins[0]->id;
 
