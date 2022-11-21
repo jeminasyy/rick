79126ff47d14306637ad1a4888740dcb2aea6b1c
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-11-21 10:08:10 +0800

    notifs

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'lastName',
         'email',
         'role',
+        'newNotifs',
         'password',
         'register_token',
         'resetToken',
