3c9b1f1de42476223f30b47c281b1bc6368fc0cf
Jemina Sy <jemina.sy.cics@ust.edu.ph>
2022-12-22 14:22:53 +0800

    revision ver2

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -6,35 +6,40 @@
         transform: rotate(0.08deg); ">
 
         <p class="attribute">How to submit a ticket?</p>
-        <p>&nbsp;&nbsp; 1. Access the Home Page and click on “Submit New Ticket”</p>
-        <p>&nbsp;&nbsp; 2. Must be a bonafide student at the College of Commerce and Business Administration.</p>
-        <p>&nbsp;&nbsp; 3. Provide your UST email address and enter the verification code that will be sent to your email.</p>
-        <p>&nbsp;&nbsp; 4. Do not spam tickets. Students are allowed to have x ongoing tickets only.</p>
-        <p>&nbsp;&nbsp; 5. Check your email for updates from time to time.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 1. &nbsp;Access the Home Page and click on “Submit New Ticket”.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 2. &nbsp;Must be a bonafide student at the College of Commerce and Business Administration.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 3. &nbsp;Provide your UST email address and enter the verification code that will be sent to your email.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 4. &nbsp;Do not spam tickets. Students are allowed to have x ongoing tickets only.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 5. &nbsp;Check your email for updates from time to time.</p>
 
         <br>
         <p class="attribute">How to reopen a ticket?</p>
-        <p>&nbsp;&nbsp; 1. You may reopen a previous ticket when you encounter the similar problem or when your problem was not fully resolved</p>
-        <p>&nbsp;&nbsp; 2. To reopen a ticket, access the Home Page and click on “Reopen Ticket”</p>
-        <p>&nbsp;&nbsp; 3. Provide your UST email address and enter the verification code that will be sent to your email.</p>
-        <p>&nbsp;&nbsp; 4. Do not spam tickets. Students are allowed to have x ongoing tickets only.</p>
-        <p>&nbsp;&nbsp; 5. Check your email for updates from time to time.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 1. &nbsp;You may reopen a previous ticket when you encounter the similar problem or when your problem was not fully resolved.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 2. &nbsp;To reopen a ticket, access the Home Page and click on “Reopen Ticket”.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 3. &nbsp;Provide your UST email address and enter the verification code that will be sent to your email.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 4. &nbsp;Do not spam tickets. Students are allowed to have x ongoing tickets only.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 5. &nbsp;Check your email for updates from time to time.</p>
 
         <br>
         <p class="attribute">Why is my ticket voided?</p>
-        <p>&nbsp;&nbsp; 1. Your ticket will be voided when the request cannot be delivered, or when the assigned Front Desk Officer deemed it as invalid. </p>
-        <p>&nbsp;&nbsp; 2. You should receive an email containing the reason for voiding.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 1. &nbsp;Your ticket will be voided when the request cannot be delivered, or when the assigned Front Desk Officer deemed it as invalid. </p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 2. &nbsp;You should receive an email containing the reason for voiding.</p>
+
+        <br>
+        <p class="attribute">Why is my ticket inactive?</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 1. &nbsp;Within 30 days after your ticket is marked as resolved, you must provide your feedback or it will be set as inactive.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 2. &nbsp;Inactive tickets can be reopened to prevent submitting duplicate tickets.</p>
 
         <br>
         <p class="attribute">My ticket is resolved but why is it not yet deducted from my ongoing tickets?</p>
-        <p>&nbsp;&nbsp; 1. Tickets are fully resolved only after you have submitted your feedback.</p>
-        <p>&nbsp;&nbsp; 2. Once your ticket is resolved, you should receive an email containing a link to the feedback form.</p>
-        <p>&nbsp;&nbsp; 3. Please provide your honest feedback to help us further improve our service.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 1. &nbsp;Tickets are fully resolved only after you have submitted your feedback.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 2. &nbsp;Once your ticket is resolved, you should receive an email containing a link to the feedback form.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 3. &nbsp;Please provide your honest feedback to help us further improve our service.</p>
 
         <br>
         <p class="attribute">How do I know the status of my ticket?</p>
-        <p>&nbsp;&nbsp; 1. Always check your email from time to time. </p>
-        <p>&nbsp;&nbsp; 2. Email updates will be sent when changes are made to the ticket. </p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 1. &nbsp;Always check your email from time to time. </p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp; 2. &nbsp;Email updates will be sent when changes are made to the ticket. </p>
         <br>
     </div>
  
